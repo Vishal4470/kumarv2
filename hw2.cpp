4f27bdeb4d02ce607f7d3b583a023d381d4e2cd1
Vishal4470 <42921083+Vishal4470@users.noreply.github.com>
2018-12-11 13:51:18 -0500

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <bits/stdc++.h>
-// Vishal Kumar
-// HW #2
-//No, you can not compute 99999999999999 + 99999999999999 on a
-//32-bit machine using integer because there would be an overflow.
-//This type can only handle a max value of about 2,000,000,000.
+/// Vishal Kumar
+/// HW #2
+/// No, you can not compute 99999999999999 + 99999999999999 on a
+/// 32-bit machine using integer because there would be an overflow.
+/// This type can only handle a max value of about 2,000,000,000.
 
 using namespace std;
 
@@ -13,7 +13,7 @@ string sumLarge (string stra, string strb)
     string str = ""; // store result
     int n1 = stra.length(), n2 = strb.length(), carry = 0;
     int difference = n2 - n1;
-    if (stra.length() > strb.length()) // makes sure str2 > str1
+    if (stra.length() > strb.length()) /// makes sure str2 > str1
         swap(stra, strb);
 
     for (int v=n1-1; v>=0; v--)
@@ -31,8 +31,8 @@ string sumLarge (string stra, string strb)
     if (carry)
         str.push_back(carry + '0');
 
-    reverse(str.begin(), str.end()); // Reverses the sum output.
-    // Derives from the stdc++.h, which is a header file.
+    reverse(str.begin(), str.end()); /// Reverses the sum output.
+    /// Derives from the stdc++.h, which is a header file.
     return str;
 }
 int main()
